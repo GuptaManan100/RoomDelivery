b9b0db65d704c432f81202b442332f048cec550a
GuptaManan100 <guptamanan100@gmail.com>
2019-04-25 08:56:05 +0530

    Order Placement Complete

--- users/completeorder.php
+++ users/completeorder.php
@@ -4,15 +4,17 @@
    //post method sent from html
    if($_SERVER["REQUEST_METHOD"]=="POST")
    {
-      $sql = "SELECT name,quantity,price FROM products;";
+      $sql = "SELECT name,quantity,price,productCode FROM products;";
       $result = $db->query($sql);
       $items = array();
       $prices = array();
+      $codes = array();
       $isprep=0;
       $amount=0;
       while($row = $result->fetch_assoc()) {
         $items[$row["name"]] = $row["quantity"];
         $prices[$row["name"]] = $row["price"];
+        $codes[$row["name"]] = $row["productCode"];
       }
    		foreach( $_POST as $stuff => $val )
       {
@@ -34,16 +36,58 @@
         {
           $items[str_replace("_"," ",$stuff)]-=$val;
           $sql = "UPDATE products SET quantity = ".$items[str_replace("_"," ",$stuff)]." WHERE name = '".str_replace("_"," ",$stuff)."';";
-          //echo $sql;
           $db->query($sql);
         }
         $amount+=$prices[str_replace("_"," ",$stuff)]*$val;
       }
+      $timeorder = date("Y-m-d H:i:s");
 
-      echo $isprep;
+      $sql = "INSERT INTO orders VALUES (null,'".$_SESSION['enrollnum']."','".$_SESSION['isStudent']."','".$_POST['Location']."',".$amount.",'".$timeorder."');";
+      $result = $db->query($sql);
+      if($result)
+      {
+        $last_id = mysqli_insert_id($db);
+        echo "<div id=\"print-content\">";
+        echo "OrderID = $last_id<br>";
+        echo "CustomerID = ".$_SESSION['enrollnum']."<br>";
+        echo "Order Time = $timeorder<br>";
+        foreach( $_POST as $stuff => $val )
+        {
+          if($stuff=="Location")
+          {
+            continue;
+          }
+          else if($val>0)
+          {
+            $sql = "INSERT INTO orderDetails VALUES (null,'".$last_id."','".$codes[str_replace("_"," ",$stuff)]."','".$val."');";
+            echo str_replace("_"," ",$stuff)."   -   $val<br>";
+            $db->query($sql);
+          }
+        }
+        echo "Amount = Rs.$amount<br>";
+        $isprep += 2;
+        $deliverTime = date('H:i:s',strtotime("+$isprep minutes",strtotime($timeorder)));
+        echo "Delivery Time = $deliverTime<br>";
+        echo "</div>";
+        echo "<input type=\"button\" onclick=\"printDiv('print-content')\" value=\"Print Order\"/>";
+      }
+      else
+      {
+        header("Location: ./placeorder.php/");
+      }
    }
    else
    {
       header("Location: ../users/");
    }
 ?>
+
+<script type="text/javascript">
+    function printDiv(divName) {
+        var printContents = document.getElementById(divName).innerHTML;
+        w=window.open();
+        w.document.write(printContents);
+        w.print();
+        w.close();
+    }
+</script>
